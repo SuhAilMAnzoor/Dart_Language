5d0d026ef22aeb10a2f169ad14cb26f9c3b7573c
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-08-08 17:10:08 +0500

    Operators in Dart

---
+++ 02.DataTypes_Operators_Equility_and_Relational_Operators_Logical_Operators_Conditional_Statement_if_else/02_Operators.dart
@@ -0,0 +1,27 @@
+//OPERATORS:
+// 1) Airthmetic Operators
+// 2) Equality and Relational Operators
+// 3) Logical Operators
+// 1) Airthmetic Operators
+// + , - , x(*) , /    these are clear 4 airthmetic
+// 5th airmetic is<  ~/  >   this will used to get interger value from divide
+
+void main() {
+  // program 2 given example below
+  int num1 = 5;
+  int num2 = 2;
+  num Result = num1 ~/ num2;
+  print(Result); // 2
+
+  // 6th reminder  %
+
+  // // Program 3
+  int num3 = 19;
+  int num4 = 4;
+  num result = num3 % num4; // will print reminder 3
+  print(result); //will print 3 reminder here
+}
+
+  // 7th ++ Increment  Skip for now
+  // 8th -- Decrement Skip for 2 or 3 classes for now
+
