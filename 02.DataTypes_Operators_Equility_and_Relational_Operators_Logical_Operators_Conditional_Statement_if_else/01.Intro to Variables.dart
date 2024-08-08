e601432663712cc1f49be839ad1b5c9c903d96bd
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-08-08 17:05:48 +0500

    DataTypes Operators Equility and Relational Operators Logical Operators Conditional Statement if else

---
+++ 02.DataTypes_Operators_Equility_and_Relational_Operators_Logical_Operators_Conditional_Statement_if_else/01.Intro to Variables.dart
@@ -0,0 +1,26 @@
+void main() {
+// >> Introducation to Variables:
+  // A Variable is a "a named space in memory" that
+  // stores values. in other words, it acts a container
+  // for values in a program.
+  // 1) Variable name can consist of letter and alphabets.
+  // 2) Keywords are not allowed to use as variable name.
+  // 3) Blanks spaces are not allowed in variable name.
+  // 4) First Character of variable should always be alphabet and cannot be digit.
+  // 5) Variable name are case sensitive i.e. UPPER and lower case are. ) significant.
+  // 6) Special Characters Like #, $ are not allowed expect the underscore(_) and the dollar ($) sign.
+
+  //  Data Types       Keyword             Description
+  //   Number     int,double,num   Numbers in dart are used to represent numeric literals
+  //   String        String        String represent a sequence of characters
+  //   boolens        bool         it represents boolen values true and false
+  //   Lists          List         it is an ordered groups of objects
+  //   Maps           Map          it represents a set of values as key-value pairs
+
+//>> Bool Data Type, we have discussed numbers and string in previous lecture.
+
+  // //Program 1
+  bool abc = true;
+  abc = 12 < 8;
+  print(abc);
+}
