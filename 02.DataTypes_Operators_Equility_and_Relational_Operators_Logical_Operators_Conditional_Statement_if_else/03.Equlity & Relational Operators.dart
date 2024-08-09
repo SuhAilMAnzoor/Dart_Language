ab40d7817a57e77493f47b866f885ecba5f6b2e7
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-08-09 18:11:44 +0500

    03.Equlity & Relational Operators

---
+++ 02.DataTypes_Operators_Equility_and_Relational_Operators_Logical_Operators_Conditional_Statement_if_else/03.Equlity & Relational Operators.dart
@@ -0,0 +1,17 @@
+// 2) Equlity & Relational Operators:
+//1st) Greater Than( >  )  , Less Than  ( < )  , Greater Than or Equal to  ( >= )  ,   Less Than or equal to ( <= )
+//2nd) Equlity   ( == )  ,   Not Equal  ( != )
+// both are clear because we have worked a lot with geater less than or eqal to
+
+void main() {
+  // Program 4  with less than or equal to
+  int age = 19;
+  bool Reesult = age <=
+      18; // false , here age is greater than 18 so we compare 18 with 19 so it is false 18 is less than 19
+  print(Reesult);
+
+  // Program 5   with not equal
+  int age1 = 19;
+  bool resultt = age1 != 18;
+  print(resultt);
+}
